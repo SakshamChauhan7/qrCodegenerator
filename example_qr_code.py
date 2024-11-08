87c93f3b69fffd98d96dc91a56b28776b6542fa8
SakshamChauhan07 <113049023+SakshamChauhan07@users.noreply.github.com>
2024-11-08 11:44:18 -0500

    adding github profile qrcode

--- example_qr_code.py
+++ example_qr_code.py
@@ -1,4 +1,19 @@
 import qrcode
-img = qrcode.make('Some data here')
-type(img)
-img.save("some_file.png")
+
+
+# img = qrcode.make('Some data here')
+# print(type(img))
+# img.save("some_file.png")
+
+
+qr = qrcode.QRCode(
+    version=2,
+    box_size=20,
+    border=2,
+)
+
+qr.add_data("https://github.com/SakshamChauhan7")
+
+img = qr.make_image(fill_color = "green", back_color = "black")
+
+img.save("gitProfile.png")
